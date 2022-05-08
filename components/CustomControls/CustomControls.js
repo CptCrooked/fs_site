08a50f84c2229ffa36f1e428f924d0df1efb6f5c
André Botha <crookdev@outlook.com>
2022-05-08 18:15:21 +0200

    Fixed nav gap at < 500px

--- components/CustomControls/CustomControls.js
+++ components/CustomControls/CustomControls.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import {
   carousel_controls,
@@ -10,35 +10,58 @@ const CustomControls = ({
   trailerData,
   fns: { back, forward },
   currentArr,
+  handleUnitChange,
+  selectRefValue,
 }) => {
+  const defaultDisabledOption = {
+    type: "Select A Unit",
+    label: "Select A Unit...",
+  };
+
+  useEffect(() => {
+    console.log([{ defaultDisabledOption }, ...trailerData]);
+  }, []);
   return (
     <div className={carousel_controls}>
-      {/* Semantic JSX */}
-      <select name="trailerType" id="trailerType">
-        {trailerData.map((trailerType) => {
-          return (
-            <option key={uuid()} value={`${trailerType.type}`}>
-              {trailerType.label}
-            </option>
-          );
-        })}
-      </select>
-      {/* UI JSX */}
-      <div role="presentation" className={custom_select}>
-        {trailerData.map((trailerType) => {
-          return (
-            <div key={uuid()} value={`${trailerType.type}`}>
-              {trailerType.label}
-            </div>
-          );
-        })}
-      </div>
       <div className={carousel_buttons}>
         <button onClick={() => back(currentArr)}>{`<`}</button>
         <button>Menu</button>
         <button>F/S</button>
         <button onClick={() => forward(currentArr)}>{`>`}</button>
       </div>
+      {/* Semantic JSX */}
+      <select
+        name="trailerType"
+        id="trailerType"
+        onChange={handleUnitChange}
+        value={selectRefValue.value}
+        defaultValue={defaultDisabledOption.type}
+      >
+        {[defaultDisabledOption, ...trailerData].map(({ type, label }, i) => {
+          return (
+            <option
+              key={`${type}${label}`}
+              value={`${type}`}
+              disabled={i < 1 && true}
+            >
+              {label}
+            </option>
+          );
+        })}
+      </select>
+      {/* UI JSX */}
+      {/* <div role="presentation" className={custom_select}>
+        <div>
+          <span>Select a unit...</span>
+        </div>
+        {trailerData.reverse().map(({ type, label }) => {
+          return (
+            <div key={uuid()} value={`${type}`}>
+              <span>{label}</span>
+            </div>
+          );
+        })}
+      </div> */}
     </div>
   );
 };
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import Image from "next/image";
 import { v4 as uuid } from "uuid";
 import {
@@ -15,9 +15,11 @@ import CustomControls from "../../components/CustomControls/CustomControls";
 const Gallery = () => {
   const { imgs } = useContext(ImageContext);
   const [index, setIndex] = useState(0);
+  const [currentImageArr, setCurrentImageArr] = useState(imgs.carouselImages);
+  const selectRefValue = useRef(null);
+  const galleryContainerRef = useRef(null);
 
   const forward = (arr) => {
-    console.log(arr);
     if (index < arr.length - 1) {
       setIndex((index) => index + 1);
     } else {
@@ -26,7 +28,6 @@ const Gallery = () => {
   };
 
   const back = (arr) => {
-    console.log(arr);
     if (index > 0) {
       setIndex((index) => index - 1);
     } else {
@@ -34,16 +35,35 @@ const Gallery = () => {
     }
   };
 
+  const handleUnitChange = (e) => {
+    selectRefValue.current = e.target.value;
+    setCurrentImageArr(imgs[`${selectRefValue.current}`]);
+  };
+
   useEffect(() => {
-    console.log(trailerData);
-  }, []);
+    let shown;
+    [...galleryContainerRef.current.children].forEach((figureEl) => {
+      if (figureEl.children[0].children[0].classList.value.length > 1) {
+        console.dir(`HAS class`);
+        console.dir(figureEl.children[0].children[0].classList);
+        shown = true;
+      } else if (
+        figureEl.children[0].children[0].classList.value.length === 0
+      ) {
+        console.dir(`DOESN'T HAVE class`);
+        console.dir(figureEl.children[0].children[0].classList);
+      }
+    });
+    console.log(`selectRefValue.current:`, selectRefValue.current);
+    console.log(`Shown:`, shown);
+  }, [currentImageArr]);
 
   return (
     <section>
-      <div className={gallery_container}>
-        {imgs.carouselImages.map((imgObj, i) => {
+      <div className={gallery_container} ref={galleryContainerRef}>
+        {currentImageArr.map((imgObj, i) => {
           return (
-            <figure className={image_wrapper} key={uuid()}>
+            <figure className={image_wrapper} key={`${imgObj.src}${i}`}>
               <Image
                 src={imgObj.src}
                 alt={imgObj.alt}
@@ -58,16 +78,18 @@ const Gallery = () => {
       </div>
       <CustomControls
         trailerData={trailerData}
-        currentArr={imgs.carouselImages}
+        currentArr={currentImageArr}
+        handleUnitChange={handleUnitChange}
         fns={{ forward, back }}
+        selectRefValue={selectRefValue}
       />
-      <p className={carousel_info}>
+      {/* <p className={carousel_info}>
         This trailer imported from USA has sides that &ldquo;pop-out&ldquo; to
         create ample space. The trailer has a bedroom, en suite bathroom, ample
         cupboards, a kitchen, a dining room, a spacious lounge and
         air-conditioning throughout. For entertainment, there is a 36&ldquo;
         Plasma TV, a DVD/CD player.
-      </p>
+      </p> */}
     </section>
   );
 };
