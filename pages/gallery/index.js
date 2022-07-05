42230139d3517475a90ea296b5a404dfb3e87c84
André Botha <crookdev@outlook.com>
2022-07-05 09:51:01 +0200

    removed erroneous import

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,10 +1,4 @@
-import { useState, useContext, useEffect, useRef } from "react";
-import Image from "next/image";
-
-import { ImageContext } from "../../contexts/ImageContext";
-import trailerData from "../../galleryData/galleryData";
 import Showcase from "../../components/Showcase/Showcase";
-import Showcase2 from "../../dev/Showcase2";
 
 const Gallery = () => {
   return <Showcase />;
